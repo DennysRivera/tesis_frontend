ce0ff831ceb833edfde14ec821541a5fafaf20e0
DennysRivera <00026919@uca.edu.sv>
2024-05-08 03:24:47 -0600

    Add: Charts

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import axios from 'axios';
+import { onMounted, onBeforeUnmount, ref } from "vue";
+import BarChart from "./components/BarChart.vue";
+import axios from "axios";
 
 const devices = ref([]);
+const intervalId = ref(null);
 
 const getData = () => {
-  axios.get("https://tesis-dashboard.onrender.com/dashboard").then((response) => {
-    console.log(response);
-    devices.value = response.data
-  }).catch((error) => {
-    console.log(error)
-  })
-}
+  axios
+    .get("https://tesis-dashboard.onrender.com/dashboard")
+    .then((response) => {
+      console.log(response);
+      devices.value = response.data;
+      console.log(devices.value[0])
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 onMounted(() => {
-  getData();
+  getData(), (intervalId.value = setInterval(getData, 60000));
+});
+
+onBeforeUnmount(() => {
+  clearInterval(intervalId.value);
 });
 </script>
 
 <template>
   <div v-for="device in devices">
-    <h1>ID del dispositivo{{ device.id }}</h1>
-    <h2>Último valor registrado: {{ device.value }}</h2>
+    <BarChart :device="device"/>
+    <h1>ID del dispositivo: {{ device.id }}</h1>
+    <h2>Valores registrados:</h2>
+    <div v-for="value in device.value">
+      <h3>valor: {{ value[0] }}</h3>
+      <h3>Hora registrada: {{ value[1] }}</h3>
+    </div>
     <h3>Hora de última actualización: {{ device.updatedAt }}</h3>
+    <br />
   </div>
 </template>
